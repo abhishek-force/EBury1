c831112ab0205a8a0b5312661df87250f54b55cd
abhishek-force <98281603+abhishek-force@users.noreply.github.com>
2022-01-31 01:18:18 +0530

    Added final changes. Removed debugs

--- force-app/main/default/lwc/existingForeignExchangeTradesLWC/existingForeignExchangeTradesLWC.js
+++ force-app/main/default/lwc/existingForeignExchangeTradesLWC/existingForeignExchangeTradesLWC.js
@@ -1,5 +1,5 @@
-import { LightningElement, wire, track } from 'lwc';
-import getTradeRecords from '@salesforce/apex/ForeignExchangeRateTradeController.getTradeRecords';
+import { LightningElement, wire } from 'lwc';
+import getTradeRecords from '@salesforce/apex/ForeignExchangeTradeController.getTradeRecords';
 
 const columns = [
     { label: 'Sell CCY', fieldName: 'SellCurrency__c', type: 'text' },
@@ -7,28 +7,25 @@ const columns = [
     { label: 'Buy CCY', fieldName: 'BuyCurrency__c', type: 'text' },
     { label: 'Buy Amount', fieldName: 'BuyAmount__c', type: 'number' },
     { label: 'Rate', fieldName: 'Rate__c', type: 'number' },
-    { label: 'Date Booked', fieldName: 'DateBooked__c', type: 'date' }
+    { label: 'Date Booked', fieldName: 'DateBookedFormula__c', type: 'datetime' }
 ];
 
 export default class ExistingForeignExchangeTradesLWC extends LightningElement {
     columns = columns;
     showTradeCreationPage = false;
     fetchedTradeRecords = [];
+    errorMessage;
     
     @wire(getTradeRecords)
     wiredTradeRecords({ error, data }) {
         if (data) {
-            console.log('In Data');
             this.fetchedTradeRecords = data;
-            console.log('After Data', data.length);
         } else if (error) {
-            console.log('In error');
-            console.log('Error=', error);
+            this.errorMessage = 'There is an error!';
         }
     }
 
     handleClick(event) {
-        console.log('Button_Clicked ', event.target.label);
         this.showTradeCreationPage = !this.showTradeCreationPage;
     }
 }
--- force-app/main/default/lwc/newForeignExchangeTradeLWC/newForeignExchangeTradeLWC.js
+++ force-app/main/default/lwc/newForeignExchangeTradeLWC/newForeignExchangeTradeLWC.js
@@ -1,9 +1,9 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
-//import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-import getExchangeRate from '@salesforce/apex/ForeignExchangeRateTradeController.getExchangeRate';
+import getExchangeRate from '@salesforce/apex/ForeignExchangeTradeController.getExchangeRate';
 
+import rateErrorLabel from '@salesforce/label/c.UserFriendlyExchangeRateProcessingError';
 
 import FOREIGN_EXCHANGE_TRADE from '@salesforce/schema/ForeignExchangeTrade__c';
 import SELL_CURRENCY from '@salesforce/schema/ForeignExchangeTrade__c.SellCurrency__c';
@@ -11,85 +11,63 @@ import SELL_AMOUNT from '@salesforce/schema/ForeignExchangeTrade__c.SellAmount__
 import BUY_CURRENCY from '@salesforce/schema/ForeignExchangeTrade__c.BuyCurrency__c';
 import BUY_AMOUNT from '@salesforce/schema/ForeignExchangeTrade__c.BuyAmount__c';
 import RATE from '@salesforce/schema/ForeignExchangeTrade__c.Rate__c';
-import DATE_BOOKED from '@salesforce/schema/ForeignExchangeTrade__c.DateBooked__c';
 
 export default class NewForeignExchangeTradeLWC extends NavigationMixin (LightningElement) {
-    //@api recordId;
-    objectApiName = 'ForeignExchangeTrade__c';
     FOREIGN_EXCHANGE_TRADE = FOREIGN_EXCHANGE_TRADE;
     SELL_CURRENCY = SELL_CURRENCY;
     SELL_AMOUNT = SELL_AMOUNT;
     BUY_CURRENCY = BUY_CURRENCY;
     BUY_AMOUNT = BUY_AMOUNT;
     RATE = RATE;
-    DATE_BOOKED = DATE_BOOKED;
 
     sellcurrency;
     buycurrency;
     sellAmount;
     buyAmount;
     conversionRate;
+    
+    errorMessage;
+    enableCreateButton = false;
+    rateError = rateErrorLabel;
 
     handleCurrencyChange(event){
         var name = event.target.fieldName;
-        console.log('currencyFieldAPI ', name);
-        console.log('currencyFieldAPI ', event.target.value);
-
-        console.log('this.SELL_CURRENCY ', this.SELL_CURRENCY);
-        console.log('this.BUY_CURRENCY ', this.BUY_CURRENCY);
-
+        
         if(name == this.SELL_CURRENCY.fieldApiName){
             this.sellcurrency = event.target.value;
         } else {
             this.buycurrency = event.target.value;
         }
 
-        console.log('sellcurrency1', this.sellcurrency);
-        console.log('buycurrency1', this.buycurrency);
-
-        if (this.sellcurrency != undefined && this.buycurrency != undefined){
-            console.log('sellcurrency2', this.sellcurrency);
-            console.log('buycurrency2', this.buycurrency);
-
+        if(this.sellcurrency != undefined && this.buycurrency != undefined){
             getExchangeRate({sellCurrencyCode: this.sellcurrency, buyCurrencyCode : this.buycurrency})
                 .then(result => {
-                    this.conversionRate = result;
-                    console.log('currencyFieldAPI X', result);
-                    this.calculateBuyAmount();
+                    if(result.errorString == undefined){
+                        this.conversionRate = result.exchangeRate;
+                        this.calculateBuyAmount();
+                    } else {
+                        this.errorMessage = this.rateError;
+                    }
                 })
                 .catch(error => {
-                    //this.error = error;
-                    console.log('error ' + error);
+                    this.errorMessage = this.rateError;
                 });
         }
     }
 
     handleSellAmountChange(event){
         this.sellAmount = event.target.value;
-        console.log('currencyFieldAPI ', this.sellAmount);
         this.calculateBuyAmount();
     }
 
     calculateBuyAmount(){
         if(this.sellAmount && this.sellcurrency && this.buycurrency) {
             this.buyAmount = this.sellAmount * this.conversionRate;
-            console.log('currencyFieldAPI ', this.buyAmount);
-        }
-    }
-
-    wiredTradeRecords({ error, data }) {
-        if (data) {
-            console.log('In Data');
-            this.fetchedTradeRecords = data;
-            console.log('After Data', data.length);
-        } else if (error) {
-            console.log('In error');
-            console.log('Error=', error);
+            this.enableCreateButton = true;
         }
     }
 
     handleSuccess(event) {
-        console.log('event.detail.id >> ', event.detail.id);
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
@@ -97,33 +75,10 @@ export default class NewForeignExchangeTradeLWC extends NavigationMixin (Lightni
                 recordId: event.detail.id
             },
         });
-
-        /*createRecord(recordInput)
-            .then(account => {
-                //move this to another method
-                const event = new ShowToastEvent({
-                    title: 'Success!',
-                    message: 'A new record is created',
-                    variant: 'success',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(event);
-            })
-            .catch(error => {
-                const event = new ShowToastEvent({
-                    title: 'Error!',
-                    message: 'There is unexpected error!',
-                    variant: 'error',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(event);
-            });*/
     }
 
     handleCancel() {
-        console.log('log1');
         const custEvent = new CustomEvent('navigateback');
-        console.log('log2');
         this.dispatchEvent(custEvent);
     }
 }
